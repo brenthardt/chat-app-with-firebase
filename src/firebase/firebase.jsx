20122403ddf1340b8d27ccafd9ca3afd07c0e6db
Baxrom <156568263+brenthardt@users.noreply.github.com>
2025-07-08 21:03:38 +0500

    Update firebase.jsx

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -2,7 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInAnonymously } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-
+  apiKey: "YOUR API KEY",
+    authDomain: "YOUR FIREBASE DOMAIN",
+    projectId: "ID",
+    storageBucket: "STORAGE ID",
+    messagingSenderId: "ID",
+    appId: "APP ID",
+    measurementId: "ID",
 };
 
 const app = initializeApp(firebaseConfig);
